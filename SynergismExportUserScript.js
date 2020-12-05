200caa580dbf849d7cd82f3657e171291f0136a5
denesmet <asobczyk@gmail.com>
2020-12-05 19:29:19 +0100

    Added max challenges

--- SynergismExportUserScript.js
+++ SynergismExportUserScript.js
@@ -1,29 +1,53 @@
 // ==UserScript==
-// @name     Synergism more info
-// @version  0.1
-// @grant    GM_addStyle
-// @match    *://pseudonian.github.io/SynergismOfficial/
-// @match    *://v1011testing.vercel.app/
+// @name         Synergism QoL
+// @namespace    http://tampermonkey.net/
+// @version      0.1.2
+// @description  Some synergism QoL improvement
+// @author       YanTovis
+// @match        https://pseudonian.github.io/SynergismOfficial/
+// @grant        none
 // ==/UserScript==
 
-function challangesDisplay() {
+let challangesDisplay = (function () {
     var chTabl = document.getElementById("challengeIcons").firstElementChild;
     var tr = document.createElement("tr");
-    tr.setAttribute("id", "challengeCompletions")
-    tr.style.fontSize = "12px"
+    tr.setAttribute("id", "challengeCompletions");
+    tr.style.fontSize = "14px";
+    tr.style.textAlign = "center";
 
     var challCompletion = player.challengecompletions;
 
-    for (var i = 1; i < challCompletion.length; i++){
+    for (var i = 1; i < challCompletion.length; i++) {
         var td = document.createElement("td");
-        td.appendChild(document.createTextNode(challCompletion[i]));
+        td.appendChild(document.createTextNode("0 / 0"));
         tr.appendChild(td);
     }
     chTabl.appendChild(tr);
+    document.getElementById("challengeDetails").style.top = "130px";
+})();
+let challengesUpdate = function () {
+    var challCompletion = player.challengecompletions;
+    for (var i = 1; i < challCompletion.length; i++) {
+        var maxCh = 0
+        var td = document.getElementById("challengeCompletions").childNodes[i - 1]
+        if (i < 6) maxCh = 25 + 5 * player.researches[66] + 925 * player.researches[105];
+        else if (i > 5 && i <= 10) maxCh = 25 + 5 * player.cubeUpgrades[29] + 2 * player.shopUpgrades.challengeExtension + 5 * player.platonicUpgrades[5] + 5 * player.platonicUpgrades[10] + 10 * player.platonicUpgrades[15];
+        else maxCh = 30 + 3 * player.platonicUpgrades[5] + 3 * player.platonicUpgrades[10] + 4 * player.platonicUpgrades[15];
+        if (challCompletion[i] == maxCh) td.style.color = "limegreen";
+        else td.style.color = "white";
+        td.textContent = challCompletion[i] + " / " + maxCh;
+    }
 }
+window.handleChallUpdate = setInterval(challengesUpdate, 1000);
 
 
-challangesDisplay();
+
+
+
+
+
+
+
 
 function exportWFCButton() {
     var newExButton = document.createElement("button");
