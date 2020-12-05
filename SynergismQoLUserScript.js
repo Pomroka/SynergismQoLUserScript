61887b4dd3edb7ef1131d96eb6b5f51d45f22a67
denesmet <asobczyk@gmail.com>
2020-12-05 22:24:18 +0100

    challenge start/exit button

--- SynergismQoLUserScript.js
+++ SynergismQoLUserScript.js
@@ -27,6 +27,73 @@ let challangesDisplay = (function () {
     document.getElementById("challengeDetails").style.top = "130px";
 })();
 
+let currentChallengeSelectd = function () {
+    var challengeSelected = document.getElementById("sartChallenge").childNodes[0].data;
+    var challengeRunningT = player.currentChallenge.transcension;
+    var challengeRunningR = player.currentChallenge.reincarnation;
+    var challengeRunningA = player.currentChallenge.ascension;
+    if (challengeSelected === "Start [No Multipliers]" && challengeRunningT === 1){ 
+        challengeSelected = "Exit [No Multipliers";
+        document.getElementById("startChallenge").onclick = "resetCheck('challenge', null, true)"
+    }
+    if (challengeSelected === "Start [No Accelerators]" && challengeRunningT === 2) {
+        challengeSelected = "Exit [No Accelerators]";
+        document.getElementById("startChallenge").onclick = "resetCheck('challenge', null, true)"
+    }   
+    if (challengeSelected === "Start [No Shards" && challengeRunningT === 3) {
+        challengeSelected = "Exit [No Shards";
+        document.getElementById("startChallenge").onclick = "resetCheck('challenge', null, true)"
+    }
+    if (challengeSelected === "Start [Cost+]" && challengeRunningT === 4) {
+        challengeSelected = "Exit [Cost+]";
+        document.getElementById("startChallenge").onclick = "resetCheck('challenge', null, true)"
+    }
+    if (challengeSelected === "Start [Reduced Diamonds]" && challengeRunningT === 5) {
+        challengeSelected = "Exit [Reduced Diamonds]";
+        document.getElementById("startChallenge").onclick = "resetCheck('challenge', null, true)"
+    }
+    if (challengeSelected === "Start <Higher Tax>" && challengeRunningR === 6) {
+        challengeSelected = "Exit <Higher Tax>";
+        document.getElementById("startChallenge").onclick = "resetCheck('reincarnationchallenge', null, true)"
+    }
+    if (challengeSelected === "Start <No Multipliers/Accelerators>" && challengeRunningR === 7) {
+        challengeSelected = "Exit <No Multipliers/Accelerators>";
+        document.getElementById("startChallenge").onclick = "resetCheck('reincarnationchallenge', null, true)"
+    }
+    if (challengeSelected === "Start <Cost++>" && challengeRunningR === 8) {
+        challengeSelected = "Exit <Cost++>";
+        document.getElementById("startChallenge").onclick = "resetCheck('reincarnationchallenge', null, true)"
+    }
+    if (challengeSelected === "Start <No Runes>" && challengeRunningR === 9) {
+        challengeSelected = "Exit <No Runes>";
+        document.getElementById("startChallenge").onclick = "resetCheck('reincarnationchallenge', null, true)"
+    }
+    if (challengeSelected === "Start <Sadistic I>" && challengeRunningR === 10) {
+        challengeSelected = "Exit <Sadistic I>";
+        document.getElementById("startChallenge").onclick = "resetCheck('reincarnationchallenge', null, true)"
+    }
+    if (challengeSelected === "Start <[(Reduced Ants)]>" && challengeRunningA === 11) {
+        challengeSelected = "Exit <[(Reduced Ants)]>";
+        document.getElementById("startChallenge").onclick = "resetCheck('ascensionChallenge')"
+    }
+    if (challengeSelected === "Start <[(No Reincarnation)]>" && challengeRunningA === 12) {
+        challengeSelected = "Exit <[(No Reincarnation)]>"
+        document.getElementById("startChallenge").onclick = "resetCheck('ascensionChallenge')"
+    };
+    if (challengeSelected === "Start <[(Tax+++)]>" && challengeRunningA === 13) {
+        challengeSelected = "Exit <[(Tax++ +)]>"
+        document.getElementById("startChallenge").onclick = "resetCheck('ascensionChallenge')"
+    };
+    if (challengeSelected === "Start <[(No Research)]>" && challengeRunningA === 14) {
+        challengeSelected = "Exit <[(No Research)]>";
+        document.getElementById("startChallenge").onclick = "resetCheck('ascensionChallenge')"
+    }
+    if (challengeSelected === "Start <[(Sadistic Challenge II)]>" && challengeRunningA === 15) {
+        challengeSelected = "Exit <[(Sadistic Challenge II)]>";
+        document.getElementById("startChallenge").onclick = "resetCheck('ascensionChallenge')"
+    }
+}
+
 let challengesUpdate = function () {
     var challCompletion = player.challengecompletions;
     for (var i = 1; i < challCompletion.length; i++) {
@@ -39,9 +106,8 @@ let challengesUpdate = function () {
         else td.style.color = "white";
         td.textContent = challCompletion[i] + " / " + maxCh;
     }
-    
+
 }
 window.handleChallUpdate = setInterval(challengesUpdate, 1000);
-
 
 
