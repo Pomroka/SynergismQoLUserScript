319909a2997bb4703f3c253ea759c7b79893f49e
denesmet <asobczyk@gmail.com>
2020-12-12 15:11:31 +0100

    More optymalization and bug fixes

--- SynergismQoL.user.js
+++ SynergismQoL.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Synergism QoL
 // @namespace    YanTovis
-// @version      0.1.9.1
+// @version      0.2.0
 // @description  Some synergism QoL improvement
 // @updateURL    https://github.com/Pomroka/SynergismQoLUserScript/raw/main/SynergismQoL.user.js
 // @author       YanTovis
@@ -10,7 +10,7 @@
 // ==/UserScript==
 
 (function () {
-    let challangesDisplay = () => {
+    function challengesDisplay() {
         let chTabl = document.getElementById("challengeIcons").firstElementChild;
         let tr = document.createElement("tr");
         tr.setAttribute("id", "challengeCompletions");
@@ -18,79 +18,102 @@
         tr.style.textAlign = "center";
         tr.style.letterSpacing = "-0.06em";
 
-        let challCompletion = player.challengecompletions;
+        let challengeCompletion = player.challengecompletions;
 
-        for (let i = 1; i < challCompletion.length; i++) {
+        for (let i = 1; i < challengeCompletion.length; i++) {
             let td = document.createElement("td");
             td.appendChild(document.createTextNode("0 / 0"));
             tr.appendChild(td);
+            let id = 'challenge' + i;
+            document.getElementById(id).onclick = event => {
+                challengeDisplay(i);
+                replaceStartChallengeButton(i);
+            };
         }
         chTabl.appendChild(tr);
         document.getElementById("challengeDetails").style.top = "130px";
     }
 
-    let replaceStartChallengeButton = () => {
-        let challengeSelected = document.getElementById("challengeName").childNodes[0].data;
-        let challengeRunning = player.currentChallenge.transcension;
-        challengeRunning += player.currentChallenge.reincarnation;
-        challengeRunning += player.currentChallenge.ascension;
-
-        const challengesNames = ["", "[No Multipliers]", "[No Accelerators]", "[No Shards]", "[Cost+]", "[Reduced Diamonds]",
-                                "<Higher Tax>", "<No Multipliers/Accelerators>", "<Cost++>", "<No Runes>", "<Sadistic I>",
-                                "<[(Reduced Ants)]>", "<[(No Reincarnation)]>", "<[(Tax+++)]>", "<[(No Research)]>", "<[(CHALLENGE)]>"];
-
-        for (let i = 1; i < challengesNames.length; i++) {
-            let chall = challengesNames[i];
-            let event;
-            if (i < 6) event = "resetCheck('challenge', null, true)";
-            if (5 < i && i < 11) event = "resetCheck('reincarnationchallenge', null, true)";
-            if (i > 10) event = "resetCheck('ascensionChallenge')";
-            if (challengeSelected.includes(chall.replace(/[\[\]\(\)\<\>I]+/g, "") + " ")){
-                if (i == 15) chall = "<[(Sadistic Challenge II)]>";
-                if (challengeRunning === i){
-                    document.getElementById("startChallenge").childNodes[0].data = "Exit " + chall;
-                    document.getElementById("startChallenge").setAttribute("onClick", event);
-                    document.getElementById("startChallenge").setAttribute("style", "background-color: plum; color: darkmagenta;");
-                    document.getElementById("challenge" + i).style.backgroundColor = "plum";
-                } else {
-                    document.getElementById("startChallenge").childNodes[0].data = "Start " + chall;
-                    document.getElementById("startChallenge").setAttribute("onClick", "toggleChallenges(triggerChallenge, false)");
-                    document.getElementById("startChallenge").setAttribute("style", "background-color: null; color: white;");
-                    document.getElementById("challenge" + i).style.backgroundColor = "green";
-                }
-            }
-            else {
-                if (challengeRunning != i){
-                    document.getElementById("challenge" + i).style.backgroundColor = null;
-                }
-            }
-        }
-    }
-
-    let challengesUpdate = () => {
-        let challCompletion = player.challengecompletions;
-        for (let i = 1; i < challCompletion.length; i++) {
-            let maxCh = 0
-            let td = document.getElementById("challengeCompletions").childNodes[i - 1]
-            if (i < 6) maxCh = 25 + 5 * player.researches[66] + 925 * player.researches[105];
-            else if (i > 5 && i <= 10) maxCh = 25 + 5 * player.cubeUpgrades[29] + 2 * player.shopUpgrades.challengeExtension + 5 * player.platonicUpgrades[5] + 5 * player.platonicUpgrades[10] + 10 * player.platonicUpgrades[15];
-            else if (i == 15) maxCh = 9001;
-            else maxCh = 30 + 3 * player.platonicUpgrades[5] + 3 * player.platonicUpgrades[10] + 4 * player.platonicUpgrades[15];
-            if (challCompletion[i] == maxCh) td.style.color = "limegreen";
-            else td.style.color = "white";
-            td.textContent = challCompletion[i] + " / " + maxCh;
-        }
-        replaceStartChallengeButton();
-    }
-
-    challangesDisplay();
     let updateChallengesInterval = 0;
-    let startUpdate = () => {
+    function startUpdate() {
         updateChallengesInterval = setInterval(challengesUpdate, 100);
     }
-    let stopUpdate = () => {
+    function stopUpdate() {
         clearInterval(updateChallengesInterval);
     }
+
+    function startChallengeEvent (i, running) {
+        if (running){
+            if (i < 6){
+                resetCheck('challenge', null, true);
+            }
+            if (5 < i && i < 11){
+                resetCheck('reincarnationchallenge', null, true);
+            }
+            if (i > 10){
+                resetCheck('ascensionChallenge');
+            }
+        } else{
+            toggleChallenges(triggerChallenge, false);
+        }
+        replaceStartChallengeButton(i);
+    }
+
+    function replaceStartChallengeButton(i) {
+        let challengeRunning = [player.currentChallenge.transcension, player.currentChallenge.reincarnation, player.currentChallenge.ascension];
+
+        const startChallengesBtnTxt = ["", "[No Multipliers]", "[No Accelerators]", "[No Shards]", "[Cost+]", "[Reduced Diamonds]",
+            "<Higher Tax>", "<No Multipliers/Accelerators>", "<Cost++>", "<No Runes>", "<Sadistic I>",
+            "<[(Reduced Ants)]>", "<[(No Reincarnation)]>", "<[(Tax+++)]>", "<[(No Research)]>", "Start <[(Sadistic Challenge II)]>"];
+
+        let startChallengeBtnTxt = 'Start ' + startChallengesBtnTxt[i];
+        let running = false;
+        let startBtnStyle = "background-color: null; color: white;";
+        let challengeIconStyle = "green";
+
+        if (challengeRunning.includes(i)) {
+            startChallengeBtnTxt = 'Exit ' + startChallengesBtnTxt[i];
+            startBtnStyle = "background-color: plum; color: darkmagenta;";
+            challengeIconStyle = "plum";
+            running = true;
+        }
+        document.getElementById("startChallenge").childNodes[0].data = startChallengeBtnTxt;
+        document.getElementById("startChallenge").onclick = event => { startChallengeEvent(i, running) };
+        document.getElementById("startChallenge").setAttribute("style", startBtnStyle);
+        document.getElementById("challenge" + i).style.backgroundColor = challengeIconStyle;
+        
+        for (let j = 1; j < startChallengesBtnTxt.length; j++) {
+            if (j != i && !challengeRunning.includes(j)) {
+                document.getElementById("challenge" + j).style.backgroundColor = null;
+            }
+        }
+    }
+
+    function challengesUpdate() {
+        let challengeRunning = player.currentChallenge.transcension + player.currentChallenge.reincarnation + player.currentChallenge.ascension;
+        if (challengeRunning > 0) {
+            let challengeCompletion = player.challengecompletions;
+            for (let i = 1; i < challengeCompletion.length; i++) {
+                let maxCh = 0;
+                let td = document.getElementById("challengeCompletions").childNodes[i - 1];
+                if (i < 6)
+                    maxCh = 25 + 5 * player.researches[66] + 925 * player.researches[105];
+                else if (i > 5 && i <= 10)
+                    maxCh = 25 + 5 * player.cubeUpgrades[29] + 2 * player.shopUpgrades.challengeExtension + 5 * player.platonicUpgrades[5] + 5 * player.platonicUpgrades[10] + 10 * player.platonicUpgrades[15];
+                else if (i == 15)
+                    maxCh = 9001;
+                else
+                    maxCh = 30 + 3 * player.platonicUpgrades[5] + 3 * player.platonicUpgrades[10] + 4 * player.platonicUpgrades[15];
+                if (challengeCompletion[i] == maxCh)
+                    td.style.color = "limegreen";
+                else
+                    td.style.color = "white";
+                td.textContent = challengeCompletion[i] + " / " + maxCh;
+            }
+        }
+    }
+
+    challengesDisplay();
 
     const tabsId = ['buildingstab', 'upgradestab', 'settingstab', 'achievementstab', 'runestab', 'challengetab', 
                     'researchtab', 'shoptab', 'anttab', 'cubetab', 'traitstab'];
@@ -103,6 +126,7 @@
         if (tabEvent == 'challenges') {
             document.getElementById(tabId).onclick = event => {
                 toggleTabs(tabEvent);
+                challengesUpdate();
                 startUpdate();
             }
         } else {
@@ -112,6 +136,4 @@
             }
         }
     }
-
-
 })();
