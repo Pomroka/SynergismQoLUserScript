1da1bc3e9bf10c4bfd0997614e502e393d6c60ef
denesmet <asobczyk@gmail.com>
2021-01-17 12:20:26 +0100

    fix incorect rate when waitTime not default 10s

--- SynergismTimeTo1e12.user.js
+++ SynergismTimeTo1e12.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Synergism Time to 1e12 Diamonds
 // @namespace    YanTovis
-// @version      0.3.2
+// @version      0.3.3
 // @description  Show in console last 10 prestige using real time and time to get 1e12 diamonds.
 // @updateURL    https://github.com/Pomroka/SynergismQoLUserScript/raw/main/SynergismTimeTo1e12.user.js
 // @author       YanTovis
@@ -14,7 +14,7 @@
 /* Usage - in browser console type: timeTo1e12Diamonds() 
 You can change default 10s time to calculate diamond rate: timeTo1e12Diamonds(time_in_seconds)  */
 
-window.timeTo1e12Diamonds = (waitTime = 10) => {
+window.timeTo1e12Diamonds = (waitTime = 10, locale="pl-PL") => {
   let resetHistory = player.history.reset;
   let diamonds = 0;
   let prestigesTime = 0;
@@ -45,13 +45,13 @@ window.timeTo1e12Diamonds = (waitTime = 10) => {
   console.log(`Wait ${waitTime} seconds...`);
   setTimeout(() => {
     const diamondsNow = player.prestigePoints.toExponential();
-    const dRate = (diamondsNow - diamondsStart) / 10;
+    const dRate = (diamondsNow - diamondsStart) / waitTime;
     const secTo1e12 = (1e12 - diamondsNow) / dRate;
     let strTo1e12 = new Date(secTo1e12 * 1000).toISOString().substr(11, 8);
     if (secTo1e12 >= 24 * 3600) {
       strTo1e12 = `${Math.floor(secTo1e12 / (24 * 3600))}day ${strTo1e12}`;
     }
-    atTime = new Date(Date.now() + secTo1e12 * 1000).toLocaleString();
+    const atTime = new Date(Date.now() + secTo1e12 * 1000).toLocaleString(locale);
     console.log(
       `Average rate after ${waitTime} seconds: ${dRate.toExponential(3)} d/s`
     );
